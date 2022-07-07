b33a48d4070c0cd77311c80ccd2d2d050c047411
Michael J. Pedersen <m.pedersen@icelus.org>
2022-07-07 09:53:51 -0400

    Initial demo project, uplifts added in

---
+++ android/app/src/main/kotlin/tech/brainframe/brainframe/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.brainframe.brainframe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import desktop_window
+import window_size
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
+}
