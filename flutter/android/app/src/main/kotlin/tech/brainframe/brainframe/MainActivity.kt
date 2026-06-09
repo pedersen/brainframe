074e8cd15c025669cc4a2d4d71f6e18500f1ecf1
Michael J. Pedersen <datacyclist@gmail.com>
2026-06-08 20:16:44 -0400

    Clear the decks for the real BrainFrame

--- flutter/android/app/src/main/kotlin/tech/brainframe/brainframe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package tech.brainframe.brainframe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter/lib/extensions/list.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:math';
-
-extension Choice<T> on List<T> {
-  T choice() {
-    final random = Random();
-    return this[random.nextInt(length)];
-  }
-}
